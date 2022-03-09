55d1fb2240079274649317cda3fb4735011a216c
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-03-09 15:24:55 +0000

    update display

--- src/Components/DisplayItem.js
+++ src/Components/DisplayItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function DisplayItem({ drink }) {
-    const ingredients = [...Array(16).keys()].slice(1).map(el => drink['strIngredient' + el] + ': ' + drink['strMeasure' + el] + ', ').filter(el => el !== 'null: null, ' )
+    const ingredients = [...Array(16).keys()].slice(1).map(el => drink['strIngredient' + el] && drink['strIngredient' + el] + ': ' + drink['strMeasure' + el] + ', ' )
     console.log(ingredients)
   return (
     <div className='displayItem'>
